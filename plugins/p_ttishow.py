dab2235103768a0df2ba1ae25d96db3b3e911f46
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-04-17 20:47:58 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('🧩 𝖲𝖴𝖯𝖯𝖮𝖱𝖳 🧩', url=f"https://t.me/{SUPPORT_CHAT}"),
-            InlineKeyboardButton('⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡', url="https://t.me/piroxbots")
+            InlineKeyboardButton('🧩 sᴜᴘᴘᴏʀᴛ 🧩', url=f"https://t.me/{SUPPORT_CHAT}"),
+            InlineKeyboardButton('⚡ᴜᴘᴅᴀᴛᴇs ⚡', url="https://t.me/Campus_Bot_Updates")
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
@@ -58,8 +58,8 @@ async def save_group(bot, message):
                                                  caption=(script.MELCOW_ENG.format(u.mention, message.chat.title)),
                                                  reply_markup=InlineKeyboardMarkup(
         [[
-            InlineKeyboardButton('🧩 𝖲𝖴𝖯𝖯𝖮𝖱𝖳 🧩', url=f"https://t.me/{SUPPORT_CHAT}"),
-            InlineKeyboardButton('⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡', url="https://t.me/piroxbots")
+            InlineKeyboardButton('🧩 sᴜᴘᴘᴏʀᴛ 🧩', url=f"https://t.me/{SUPPORT_CHAT}"),
+            InlineKeyboardButton('⚡ᴜᴘᴅᴀᴛᴇs⚡', url="https://t.me/Campus_Bot_Updates")
         ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
@@ -84,8 +84,8 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('🧩 𝖲𝖴𝖯𝖯𝖮𝖱𝖳 🧩', url=f"https://t.me/{SUPPORT_CHAT}"),
-            InlineKeyboardButton('⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡', url="https://t.me/piroxbots")
+            InlineKeyboardButton('🧩 sᴜᴘᴘᴏʀᴛ 🧩', url=f"https://t.me/{SUPPORT_CHAT}"),
+            InlineKeyboardButton('⚡ᴜᴘᴅᴀᴛᴇs⚡', url="https://t.me/Campus_Bot_Updates")
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
@@ -124,8 +124,8 @@ async def disable_chat(bot, message):
     await message.reply('Chat Successfully Disabled')
     try:
         buttons = [[
-            InlineKeyboardButton('🧩 𝖲𝖴𝖯𝖯𝖮𝖱𝖳 🧩', url=f"https://t.me/{SUPPORT_CHAT}"),
-            InlineKeyboardButton('⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡', url="https://t.me/piroxbots")
+            InlineKeyboardButton('🧩 sᴜᴘᴘᴏʀᴛ 🧩', url=f"https://t.me/{SUPPORT_CHAT}"),
+            InlineKeyboardButton('⚡ ᴜᴘᴅᴀᴛᴇs ⚡', url="https://t.me/Campus_Bot_Updates")
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
